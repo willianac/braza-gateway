1845c1e32736a577b6e81454647c562b13370f8d
Willian Almeida <willianac01@gmail.com>
2024-10-02 12:03:33 -0300

    feat: allow get transactions report using only account id

--- src/controllers/getTransactionsController.ts
+++ src/controllers/getTransactionsController.ts
@@ -1,21 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { getDailyTransaction } from "../services/getDailyTransactions.js";
 import { js2xml } from "xml-js";
+import { getMerchantByAccountId } from "../utils/getMerchantByAccountId.js";
 
 export async function getTransactionsController(req: Request, res: Response, next: NextFunction) {
   try {
-    const { x_Account_Number, x_Api_key, x_Application_Id } = req.body
-    if(!x_Account_Number || !x_Api_key || !x_Application_Id) return res.status(400).send("missing required parameters")
-    
-    const result = await getDailyTransaction({
-      accountNumber: x_Account_Number || process.env.ACCOUNT_NUMBER,
-      apiKey: x_Api_key || process.env.API_KEY,
-      applicationId: x_Application_Id || process.env.APPLICATION_ID
+    const { x_Account_Number, x_Api_key, x_Application_Id, inJson } = req.body
+    //if(!x_Account_Number || !x_Api_key || !x_Application_Id) return res.status(400).send("missing required parameters")
+    let merchant;
+
+    if(!x_Api_key && !x_Application_Id) merchant = getMerchantByAccountId(x_Account_Number)
+
+    let result;
+    result = await getDailyTransaction({
+      accountNumber: x_Account_Number,
+      apiKey: x_Api_key || merchant?.api_Key,
+      applicationId: x_Application_Id || merchant?.application_id
     });
 
-    const xmlData = js2xml(result, { compact: true, ignoreComment: true })
-    res.type("text/xml")
-    res.send(xmlData)
+    if(!inJson) {
+      result = js2xml(result, { compact: true, ignoreComment: true })
+      res.type("text/xml")
+    }
+    res.send(result)
   } catch (error) {
     next(error)
   }
