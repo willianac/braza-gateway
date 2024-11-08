b2422750529a9c41592f8ae946439e619bade3a9
Willian Almeida <willianac01@gmail.com>
2024-11-08 13:53:08 -0300

    refactor: rename success response type

--- src/services/getQuotation.ts
+++ src/services/getQuotation.ts
@@ -1,13 +1,13 @@
 import fetch, { Headers } from "node-fetch";
 import { RequestError } from "../types/BrazaRequestError.js";
 
-type SuccessResponse = {
+export type GetBrazaQuotationSuccessResponse = {
   quotation: number
   quotation_original: number
   time_exp: string
 }
 
-export async function getQuotation(params: URLSearchParams): Promise<SuccessResponse | RequestError> {
+export async function getQuotation(params: URLSearchParams): Promise<GetBrazaQuotationSuccessResponse | RequestError> {
   const headers = new Headers()
   headers.append("x-api-key", process.env.API_KEY as string)
   headers.append("x-application-id", process.env.APPLICATION_ID as string)
@@ -24,6 +24,6 @@ export async function getQuotation(params: URLSearchParams): Promise<SuccessResp
     return errorData
   }
 
-  const data = await res.json() as SuccessResponse
+  const data = await res.json() as GetBrazaQuotationSuccessResponse
   return data
 }
