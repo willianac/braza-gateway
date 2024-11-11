e687b4a297f3e534ec42305870c56487fe60799a
Willian Almeida <willianac01@gmail.com>
2024-11-11 17:19:18 -0300

    feat: create a service to retrieve balance in a faster way

--- src/controllers/getBalanceController.ts
+++ src/controllers/getBalanceController.ts
@@ -1,22 +1,21 @@
 import { NextFunction, Request, Response } from "express";
-import { getDailyTransaction } from "../services/getDailyTransactions.js";
 import { js2xml } from "xml-js";
+import { getBalance } from "../services/getBalance.js";
 
 export async function getBalanceController(req: Request, res: Response, next: NextFunction) {
   try {
     const { x_Account_Number, x_Api_key, x_Application_Id } = req.body
-    const result = await getDailyTransaction({
+    const result = await getBalance({
       accountNumber: x_Account_Number,
       apiKey: x_Api_key,
       applicationId: x_Application_Id
     })
-    const balance = { balance: result.balance }
+
+    const balance = { balance: result }
     const xmlData = js2xml(balance, { compact: true, ignoreComment: true })
     res.type("text/xml")
     res.status(200).send(xmlData)
   } catch (error) {
-    //testando enviar como resposta para o xpresso erro com status 200.
-    res.status(500).send((error as any).message)
-    //next(error)
+    next(error)
   }
 }
