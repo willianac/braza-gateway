497e3bcbaabb1bba6d744b31dd4dac032f52daf4
Willian Almeida <willianac01@gmail.com>
2024-12-09 09:20:07 -0300

    feat: create function that makes xpresso invoice creation

---
+++ src/services/createInvoice.ts
@@ -0,0 +1,23 @@
+import { CreateXpressoInvoicePayload } from "../types/CreateXpressoInvoicePayload";
+import fetch, { Headers } from "node-fetch";
+
+function makeUrl(baseUrl: string, data: Record<string, string | number | boolean>) {
+  const params = new URLSearchParams()
+  for(const [key, value] of Object.entries(data)) {
+    params.append(key, value.toString())
+  }
+  return `${baseUrl}?${params.toString()}`
+}
+
+export async function createXpressoInvoice(payload: CreateXpressoInvoicePayload) {
+  const BASE_URL = `https://api.moneytransmittersystem.com/mts/${payload.endpoint}/mobileApp/xpappstart.cfm/`
+  const { endpoint, ...payloadWithoutEndpoint } = payload
+
+  const FULL_URL = makeUrl(BASE_URL, payloadWithoutEndpoint)
+
+  const res = await fetch(FULL_URL, {
+    method: "GET"
+  })
+  console.log("resposta do create invoice:")
+  console.log(res)
+}
---
+++ src/types/CreateXpressoInvoicePayload.ts
@@ -0,0 +1,13 @@
+export type CreateXpressoInvoicePayload = {
+  sessionKey: string
+  receiverId: string
+  accountId: string
+  amount: string
+  nacional: string
+  purposeId: string
+  language: string
+  payorId: string
+  paymentId: string
+  geolocation: string
+  endpoint: string
+}
